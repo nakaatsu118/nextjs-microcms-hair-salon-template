af080557b19b4ce67585888858b21a3318a4380e
nakaatsu <nakashima@microcms.co.jp>
2023-05-22 21:25:57 +0900

    予約ボタン仮リンクを変更

--- components/ReserveButton/index.tsx
+++ components/ReserveButton/index.tsx
@@ -4,7 +4,7 @@ import styles from './Home.module.css';
 export default function ReserveButton() {
   return (
     // TODO: 予約ボタンのリンク先を変更する
-    <a className={styles.reserveButton} href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+    <a className={styles.reserveButton} href="/" target="_blank" rel="noopener noreferrer">
       <div className={styles.reserveIcon}>
         <img src={'/calendar.svg'} alt="予約" />
       </div>
